11f9269ff2a44c5e3f77a67f0090a0cfa6ede207
ssszp <37320263+ssszp@users.noreply.github.com>
2025-03-09 18:46:02 +0800

    fix
     修复CoolQueue装饰器worker参数类型错误

--- task/src/decorator/queue.ts
+++ task/src/decorator/queue.ts
@@ -4,7 +4,7 @@ import {
   saveClassMetadata,
   saveModule,
 } from '@midwayjs/core';
-import { JobsOptions } from 'bullmq';
+import { JobsOptions, WorkerOptions } from 'bullmq';
 
 export const COOL_TASK_KEY = 'decorator:cool:task';
 
@@ -12,7 +12,7 @@ export function CoolQueue(
   config = { type: 'comm', queue: {}, worker: {} } as {
     type?: 'comm' | 'getter' | 'noworker' | 'single';
     queue?: JobsOptions;
-    worker?: WorkerOptions;
+    worker?: Omit<WorkerOptions, 'connection' | 'prefix'>;
   }
 ): ClassDecorator {
   return (target: any) => {
