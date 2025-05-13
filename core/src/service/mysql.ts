a402464cc27368023001a527d04857a863744c20
COOL <team@cool-js.com>
2025-05-13 16:00:45 +0800

    fix entityRenderPage

--- core/src/service/mysql.ts
+++ core/src/service/mysql.ts
@@ -162,7 +162,7 @@ export abstract class BaseMysqlService {
       find.addOrderBy(order, sort.toUpperCase());
     }
     return {
-      list: await dataFind.getMany(),
+      list: await dataFind.getRawMany(),
       pagination: {
         page: parseInt(page),
         size: parseInt(size),
--- core/src/service/sqlite.ts
+++ core/src/service/sqlite.ts
@@ -193,7 +193,7 @@ export abstract class BaseSqliteService {
       find.addOrderBy(order, sort.toUpperCase());
     }
     return {
-      list: await dataFind.getMany(),
+      list: await dataFind.getRawMany(),
       pagination: {
         page: parseInt(page),
         size: parseInt(size),
